ca0a54457bf74cc41cca56bc78202c49448a57d0
veemish <vlikunama@gmail.com>
2019-08-05 12:53:42 +0300

    git folder present  :art:

---
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -0,0 +1,20 @@
+package com.example.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+
+
+}
+
