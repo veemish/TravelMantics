9eeaffa9d3a62e31498ee3dea3d5dc4f7525713e
veemish <vlikunama@gmail.com>
2019-08-07 14:34:05 +0300

    Doc: Show save menu option :zap: Issue #2 The save menu option, to save the inserted data in the insert activity.

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -7,12 +7,32 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class InsertActivity extends AppCompatActivity {
+    private FirebaseDatabase   mFirebaseDatabase;
+    private DatabaseReference  mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+       mFirebaseDatabase = FirebaseDatabase.getInstance();
+       mDatabaseReference = mFirebaseDatabase.getReference().child("travel_deals");
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return
+                true;
     }
 
 
